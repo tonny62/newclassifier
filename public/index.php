7a75e6631fd794aa89103f86cc683fe58c952313
tonny62 <ton_kriddanai@hotmail.com>
2018-02-04 14:44:27 +0700

    push local files

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+require '../config/conf.php';
+
+$app = new \Slim\App(['settings'=>$config]);
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello $name");
+
+    return $response;
+});
+$app->run();
