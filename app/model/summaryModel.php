e93c02ab538fa4dea6a45ddaf449229bae35b9f2
tonny62 <ton_kriddanai@hotmail.com>
2018-03-13 12:17:55 +0700

    fix comma in job tag

--- app/model/summaryModel.php
+++ app/model/summaryModel.php
@@ -24,39 +24,39 @@
                     table1.idjob,
                     SUBSTRING_INDEX(table1.myconcat,',',1) AS tag1,
                     CASE
-                        WHEN table1.myconcat LIKE '%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',2),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',2),',',-1)
                         ELSE NULL
                         END AS 'tag2',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',3),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',3),',',-1)
                         ELSE NULL
                         END AS 'tag3',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',4),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',4),',',-1)
                         ELSE NULL
                         END AS 'tag4',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',5),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',5),',',-1)
                         ELSE NULL
                         END AS 'tag5',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',6),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',6),',',-1)
                         ELSE NULL
                         END AS 'tag6',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',7),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',7),',',-1)
                         ELSE NULL
                         END AS 'tag7',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',8),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',8),',',-1)
                         ELSE NULL
                         END AS 'tag8',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',9),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',9),',',-1)
                         ELSE NULL
                         END AS 'tag9',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%,%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',10),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',10),',',-1)
                         ELSE NULL
                         END AS 'tag10'
                 FROM
@@ -109,39 +109,39 @@
                     table1.idjob,
                     SUBSTRING_INDEX(table1.myconcat,',',1) AS tag1,
                     CASE
-                        WHEN table1.myconcat LIKE '%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',2),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',2),',',-1)
                         ELSE NULL
                         END AS 'tag2',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',3),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',3),',',-1)
                         ELSE NULL
                         END AS 'tag3',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',4),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',4),',',-1)
                         ELSE NULL
                         END AS 'tag4',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',5),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',5),',',-1)
                         ELSE NULL
                         END AS 'tag5',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',6),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',6),',',-1)
                         ELSE NULL
                         END AS 'tag6',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',7),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',7),',',-1)
                         ELSE NULL
                         END AS 'tag7',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',8),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',8),',',-1)
                         ELSE NULL
                         END AS 'tag8',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',9),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',9),',',-1)
                         ELSE NULL
                         END AS 'tag9',
                     CASE
-                        WHEN table1.myconcat LIKE '%,%,%,%,%,%,%,%,%,%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',10),',',-1)
+                        WHEN table1.myconcat LIKE '|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|,|%|' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(table1.myconcat,',',10),',',-1)
                         ELSE NULL
                         END AS 'tag10'
                 FROM
